c35f39803058d38b5ed8edf84830548eefc961eb
dinis <dinishote@gmail.com>
2026-05-06 20:48:11 +0800

    Fixed the React Configuration

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import LoginPage from "./Pages/Login/Login";
 
-ReactDOM.createRoot(document.getElementById("app")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("app")!).render(
+    <LoginPage />
+);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <title>React App</title>
 
     @viteReactRefresh
-    @vite(['resources/js/app.tsx'])
+    @vite(['resources/css/app.css', 'resources/js/app.tsx'])
 </head>
 <body>
     <div id="app"></div>
